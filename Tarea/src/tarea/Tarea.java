82530d8804389cf81827c60ea69de410067727cb
Juan José Cruz Cornejo <juan.cruz.sc@itszapopan.edu.mx>
2018-09-04 14:21:35 -0500

    segundo saludo de edith

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -7,6 +7,7 @@ public class Tarea {
     public static void main(String[] args) {
        System.out.println("Hola grupo, ");
        System.out.println("Saludos de Edith");
+       System.out.println("Que pases buenas tardes");
     }
     
 }
